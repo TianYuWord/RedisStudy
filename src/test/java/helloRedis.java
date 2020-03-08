ca0a1b3746c3a02cfe706365ae619db5abe8b4b5
tianyu <2949852842@qq.com>
2020-03-08 23:12:23 +0800

    Jedis 在Java中的使用

--- src/test/java/helloRedis.java
+++ src/test/java/helloRedis.java
@@ -1,6 +1,9 @@
 import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPoolConfig;
 
 
 public class helloRedis {
@@ -8,16 +11,34 @@ public class helloRedis {
     private static final String host = "192.168.0.105";
     private static final Integer port = 6379;
     private static final String password = "123456";
+    Jedis jedis = null;
+
+    @Before
+    public void init(){
+        jedis = new Jedis(host,port);
+        jedis.auth("123456");
+    }
 
     @Test
     public void HelloTest(){
-        Jedis jedis = new Jedis(host,port);
-        jedis.auth("123456");
-
         jedis.set("name","helloRedis");
         String name = jedis.get("name");
         System.out.println(name);
+    }
 
+    @Test
+    public void RedisExists(){
+        String key = "name";
+        jedis.set(key,"数据库的内容");
+        if(jedis.exists(key)){
+            System.out.println(jedis.get(key));
+        }
+    }
+
+
+
+    @After
+    public void destroy(){
         jedis.close();
     }
 }
